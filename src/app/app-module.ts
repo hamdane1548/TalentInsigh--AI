e046caa01e1f06405d5ec5f2cd191c657bac30e6
hamdane1548 <oussamahamdane80@gmail.com>
2025-12-10 22:13:18 +0100

    Navbar component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './component/navbar/navbar';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar
   ],
   imports: [
     BrowserModule,
