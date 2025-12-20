826b98be93e3a26a57bd70daca15ed4760e47bf4
aymen <aymen@gmail.com>
2025-12-20 15:04:00 +0100

    V1 About Us Page

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './ui/home/home';
+import {About} from './ui/about/about';
 
 const routes: Routes = [
-  {path:"",component:Home}
+  {path:"",component:Home},
+  {path:'about', component: About }
 ];
 
 @NgModule({
